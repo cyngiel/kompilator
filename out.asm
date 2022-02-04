fb0ef841acbed0b4bf8aaf1051c14f2f6dd63d30
cyngiel <pstefanski07@gmail.com>
2022-02-04 01:42:54 +0100

    real math working

--- out.asm
+++ out.asm
@@ -1,12 +1,11 @@
 jump.i  #lab0
 lab0:
-mov.i	#2, 0
-mov.i	#3, 8
-mov.i	#10, 4
-mov.i	#5, 12
-mul.i	0, 8, 16
-mul.i	4, 12, 20
-add.i	16, 20, 24
-mov.i	24, 8
-write.i	8
+mov.r		#2.5, 0
+mov.r		#3.0, 16
+mov.r		#10.3, 8
+mov.r		#5.0, 24
+inttoreal.i	#3, 40
+mul.r		#2.5, 40, 32
+mov.r		32, 16
+write.r		16
 exit
